068e23d5ba1333f0587125cdf85ef1b2a7c9696f
Dmitrii Gagarin <gagriu@yandex.ru>
2026-05-05 22:56:56 +0300

    Initial Commit

---
+++ TravelTo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TravelTo
+//
+//  Created by Dmitry Fatsievich on 05.05.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
